edcb91eae77d2aad68588683a082e2617eec0452
Jean Boussier <jean.boussier@gmail.com>
2014-03-08 13:19:08 -0500

    Also split spec files

---
+++ spec/scope_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+describe 'querying' do
+
+  describe '.first' do
+
+    it 'returns the first country' do
+      country = Country.first
+      expect(country.id).to be == 1
+    end
+
+    it 'can be called on any scope' do
+      country = Country.where(name: 'France').first
+      expect(country.id).to be == 2
+    end
+
+  end
+
+  describe '.last' do
+
+    it 'returns the first country' do
+      country = Country.last
+      expect(country.id).to be == 2
+    end
+
+    it 'can be called on any scope' do
+      country = Country.where(name: 'Canada').last
+      expect(country.id).to be == 1
+    end
+
+  end
+
+  describe '.find' do
+
+    it 'allow to find records by id' do
+      country = Country.find(1)
+      expect(country.id).to be == 1
+      expect(country.name).to be == 'Canada'
+    end
+
+    it 'raises a StaticRecord::RecordNotFound error if the id do not exist' do
+      expect {
+        Country.find(42)
+      }.to raise_error(StaticRecord::RecordNotFound)
+    end
+
+  end
+
+  describe '.find_by_id' do
+
+    it 'allow to find records by id' do
+      country = Country.find_by_id(1)
+      expect(country.id).to be == 1
+      expect(country.name).to be == 'Canada'
+    end
+
+    it 'returns nil if the id do not exist' do
+      country = Country.find_by_id(42)
+      expect(country).to be_nil
+    end
+
+  end
+
+  describe '.where' do
+
+    it 'returns the records that match given criterias' do
+      countries = Country.where(name: 'France')
+      expect(countries.length).to be == 1
+      expect(countries.first.name).to be == 'France'
+    end
+
+    it 'is chainable' do
+      countries = Country.where(name: 'France').where(id: 1)
+      expect(countries).to be_empty
+    end
+
+  end
+
+  describe '.exists?' do
+
+    it 'returns true if query match at least one record' do
+      scope = Country.where(name: 'France')
+      expect(scope).to exist
+    end
+
+    it 'returns true if query match no records' do
+      scope = Country.where(name: 'France', id: 42)
+      expect(scope).to_not exist
+    end
+
+  end
+
+end
--- spec/static_record_spec.rb
+++ spec/static_record_spec.rb
@@ -2,98 +2,6 @@ require 'spec_helper'
 
 describe StaticRecord::Base do
 
-  describe 'querying' do
-
-    describe '.first' do
-
-      it 'returns the first country' do
-        country = Country.first
-        expect(country.id).to be == 1
-      end
-
-      it 'can be called on any scope' do
-        country = Country.where(name: 'France').first
-        expect(country.id).to be == 2
-      end
-
-    end
-
-    describe '.last' do
-
-      it 'returns the first country' do
-        country = Country.last
-        expect(country.id).to be == 2
-      end
-
-      it 'can be called on any scope' do
-        country = Country.where(name: 'Canada').last
-        expect(country.id).to be == 1
-      end
-
-    end
-
-    describe '.find' do
-
-      it 'allow to find records by id' do
-        country = Country.find(1)
-        expect(country.id).to be == 1
-        expect(country.name).to be == 'Canada'
-      end
-
-      it 'raises a StaticRecord::RecordNotFound error if the id do not exist' do
-        expect {
-          Country.find(42)
-        }.to raise_error(StaticRecord::RecordNotFound)
-      end
-
-    end
-
-    describe '.find_by_id' do
-
-      it 'allow to find records by id' do
-        country = Country.find_by_id(1)
-        expect(country.id).to be == 1
-        expect(country.name).to be == 'Canada'
-      end
-
-      it 'returns nil if the id do not exist' do
-        country = Country.find_by_id(42)
-        expect(country).to be_nil
-      end
-
-    end
-
-    describe '.where' do
-
-      it 'returns the records that match given criterias' do
-        countries = Country.where(name: 'France')
-        expect(countries.length).to be == 1
-        expect(countries.first.name).to be == 'France'
-      end
-
-      it 'is chainable' do
-        countries = Country.where(name: 'France').where(id: 1)
-        expect(countries).to be_empty
-      end
-
-    end
-
-    describe '.exists?' do
-
-      it 'returns true if query match at least one record' do
-        scope = Country.where(name: 'France')
-        expect(scope).to exist
-      end
-
-      it 'returns true if query match no records' do
-        scope = Country.where(name: 'France', id: 42)
-        expect(scope).to_not exist
-      end
-
-    end
-
-  end
-
   describe '#==' do
 
     it 'returns true if both instances are from the same class and have the same id' do
@@ -156,8 +64,6 @@ describe StaticRecord::Base do
     it 'considers not `nil` objects as present' do
       expect(present.king?).to be_true
     end
-
-    
 
   end
 
