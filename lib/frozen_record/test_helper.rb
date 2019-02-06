4eb2d2c3f7391a3dbed15bbe8b3de4eac04ae2df
Christopher Butcher <christopher.butcher@shopify.com>
2019-02-06 14:35:11 -0500

    WIP Fix test helper flakiness

--- lib/frozen_record/test_helper.rb
+++ lib/frozen_record/test_helper.rb
@@ -1,14 +1,12 @@
 module FrozenRecord
   module TestHelper
-    NoFixturesLoaded = Class.new(StandardError)
-
     class << self
       def load_fixture(model_class, alternate_base_path)
         @cache ||= {}
 
         ensure_model_class_is_frozenrecord(model_class)
 
-        return if @cache.key?(model_class)
+        # return if @cache.key?(model_class)
 
         @cache[model_class] = {
           old_base_path: model_class.base_path,
@@ -23,6 +21,7 @@ module FrozenRecord
         @cache.each do |model_class, cached_values|
           model_class.base_path = cached_values[:old_base_path]
           model_class.load_records
+          model_class.eager_load!
           model_class.auto_reloading = cached_values[:old_auto_reloading]
         end
 
