67d20daaaef0628b7e5cccf59788cdeefec58731
Jean Boussier <jean.boussier@gmail.com>
2021-04-29 11:02:43 +0200

    Support new attribute generation in Rails 7.0.0.alpha

--- lib/frozen_record/compact.rb
+++ lib/frozen_record/compact.rb
@@ -25,14 +25,8 @@ module FrozenRecord
         end
       end
 
-      if ActiveModel.gem_version >= Gem::Version.new('6.1.0.alpha')
-        def define_method_attribute(attr, owner:)
-          owner << "attr_reader #{attr.inspect}"
-        end
-      else
-        def define_method_attribute(attr)
-          generated_attribute_methods.attr_reader(attr)
-        end
+      def define_method_attribute(attr, **)
+        generated_attribute_methods.attr_reader(attr)
       end
 
       attr_reader :_attributes_cache
