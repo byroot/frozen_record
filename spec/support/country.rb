5fef9bb944d6a2efd98ac67e3df71b2f1b941f3e
Richard Platel <richard.platel@shopify.com>
2023-02-08 20:32:58 +0000

    Add deseriailzation of serialized fields

--- spec/support/country.rb
+++ spec/support/country.rb
@@ -1,8 +1,25 @@
+class CurrencyCode
+  class << self
+    def load(value)
+      value.to_sym
+    end
+  end
+end
+
+class TectonicString < String
+  class << self
+    alias_method :load, :new
+  end
+end
+
 class Country < FrozenRecord::Base
-  self.default_attributes = { contemporary: true, available: true }
+  self.default_attributes = { contemporary: true, available: true, currency_code: CurrencyCode.load('EUR') }
 
   add_index :name, unique: true
   add_index :continent
+
+  attribute :currency_code, CurrencyCode
+  attribute :continent, TectonicString
 
   def self.republics
     where(king: nil)
